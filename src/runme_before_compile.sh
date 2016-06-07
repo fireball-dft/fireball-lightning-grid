8f2dc1371a08710cefb63c349ce7bde8a772ebfd
Guillermo <gufranco@mail.wvu.edu>
2016-06-06 23:01:25 -0400

    New symlinks

--- src/runme_before_compile.sh
+++ src/runme_before_compile.sh
@@ -2,7 +2,7 @@
 
 MASTER=fireball-master
 
-for i in Makefile MACHINES j.ASSEMBLERS e.FDATA g.XC_FUNCTIONALS h.SOLVESH l.SCF o.OUTPUT u.UTIL libs include p.HARRIS d.FUNCTIONS_EXTRA a.GLOBAL b.FUNCTIONS c.SYSTEM
+for i in Makefile MACHINES j.ASSEMBLERS e.FDATA g.XC_FUNCTIONALS h.SOLVESH l.SCF o.OUTPUT u.UTIL libs include p.THEORY d.FUNCTIONS_EXTRA a.GLOBAL b.FUNCTIONS c.SYSTEM
 do
     if [ -e $i ]
     then
